daf0f49db4a0231e606168000ca911c5c40ee024
kumulynja <b@kumuly.dev>
2023-10-28 21:17:20 +0200

    feat: move to pocket button

--- lib/features/sales/sales_screen.dart
+++ lib/features/sales/sales_screen.dart
@@ -5,8 +5,10 @@ import 'package:kumuly_pocket/constants.dart';
 import 'package:kumuly_pocket/enums/bitcoin_unit.dart';
 import 'package:kumuly_pocket/enums/local_currency.dart';
 import 'package:kumuly_pocket/features/sales/sales_controller.dart';
+import 'package:kumuly_pocket/theme/custom_theme.dart';
 import 'package:kumuly_pocket/theme/palette.dart';
 import 'package:kumuly_pocket/widgets/buttons/focus_mark_icon_button.dart';
+import 'package:kumuly_pocket/widgets/buttons/primary_text_button.dart';
 import 'package:kumuly_pocket/widgets/headers/wallet_header.dart';
 import 'package:kumuly_pocket/widgets/icons/dynamic_icon.dart';
 import 'package:kumuly_pocket/widgets/lists/transaction_list.dart';
@@ -20,6 +22,7 @@ class SalesScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final copy = AppLocalizations.of(context)!;
+    final textTheme = Theme.of(context).textTheme;
     double screenWidth = MediaQuery.of(context).size.width;
 
     final salesController = ref.watch(salesControllerProvider);
@@ -62,45 +65,28 @@ class SalesScreen extends ConsumerWidget {
                 balanceInFiat: balanceInFiat,
                 fiatCurrency: fiatCurrency,
                 actions: [
-                  FocusMarkIconButton(
-                    size: 44,
-                    focusCornerLength: 10,
-                    focusRadius: 10,
-                    focusStrokeWidth: 2.0,
-                    focusColor: Palette.neutral[120]!,
-                    icon: const DynamicIcon(
-                      icon: 'assets/icons/send_receive_arrows.svg',
+                  PrimaryTextButton(
+                    text: copy.moveFundsToPocket,
+                    textStyle: textTheme.body3(
+                      Palette.russianViolet[100],
+                      FontWeight.w700,
+                      wordSpacing: 0,
                     ),
-                    iconSize: 24.0,
-                    iconColor: Palette.neutral[120]!,
-                    onPressed: () {
-                      showModalBottomSheet(
-                        useRootNavigator: true,
-                        elevation: 0,
-                        isScrollControlled: true,
-                        backgroundColor: Colors.white,
-                        context: context,
-                        builder: (context) => ActionsBottomSheetModal(
-                          actionIcons: [
-                            DynamicIcon(
-                              icon: 'assets/icons/pocket.svg',
-                              color: Palette.neutral[80]!,
-                            ),
-                          ],
-                          actionTitles: [
-                            copy.moveToPocket,
-                          ],
-                          actionOnPresseds: [
-                            () {},
-                          ],
-                        ),
-                      );
-                    },
+                    trailingIcon: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Palette.russianViolet[100],
+                      size: 12.0,
+                    ),
+                    onPressed: () {},
                   ),
                 ],
               ),
               const SizedBox(
-                height: kExtraLargeSpacing,
+                height: kLargeSpacing,
+              ),
+              const SalesGraph(),
+              const SizedBox(
+                height: kMediumSpacing,
               ),
               TransactionList(
                 title: copy.recentTransactions.toUpperCase(),
@@ -112,6 +98,23 @@ class SalesScreen extends ConsumerWidget {
           width: screenWidth,
         ),
       ]),
+    );
+  }
+}
+
+class SalesGraph extends StatelessWidget {
+  const SalesGraph({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Palette.neutral[30]!),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      height: 200,
+      child: Container(),
     );
   }
 }
