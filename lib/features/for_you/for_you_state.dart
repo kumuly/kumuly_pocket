9392e6631aad6fd5893e1645e1287c5736d87ce2
kumulynja <b@kumuly.dev>
2023-12-06 23:43:25 +0100

    feat: for you promo cards and grid improvements

--- lib/features/for_you/for_you_state.dart
+++ lib/features/for_you/for_you_state.dart
@@ -1,24 +1,28 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:kumuly_pocket/view_models/promo.dart';
 
 @immutable
 class ForYouState extends Equatable {
   const ForYouState({
     required this.tabs,
     required this.selectedTab,
+    required this.promos,
   });
 
   final List<String> tabs;
   final int selectedTab;
-  //final List<Product> products;
+  final List<Promo> promos; // Todo: change to product model
 
   ForYouState copyWith({
     List<String>? tabs,
     int? selectedTab,
+    List<Promo>? promos,
   }) {
     return ForYouState(
       tabs: tabs ?? this.tabs,
       selectedTab: selectedTab ?? this.selectedTab,
+      promos: promos ?? this.promos,
     );
   }
 
@@ -26,5 +30,6 @@ class ForYouState extends Equatable {
   List<Object?> get props => [
         tabs,
         selectedTab,
+        promos,
       ];
 }
