31f6fff058f313d050f8d35062808954e3b024cb
kumulynja <b@kumuly.dev>
2024-01-03 20:04:37 +0100

    copy: add copy labels for landing stories

--- lib/features/landing/landing_flow.dart
+++ lib/features/landing/landing_flow.dart
@@ -5,12 +5,14 @@ import 'package:kumuly_pocket/features/landing/pages/landing_screen.dart';
 import 'package:kumuly_pocket/features/landing/pages/landing_story_screen.dart';
 import 'package:kumuly_pocket/widgets/icons/dynamic_icon.dart';
 import 'package:kumuly_pocket/widgets/page_views/page_view_controller.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LandingFlow extends ConsumerWidget {
   const LandingFlow({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final copy = AppLocalizations.of(context)!;
     final pageController = ref.watch(
       pageViewControllerProvider(
         kLandingFlowPageViewId,
@@ -25,45 +27,42 @@ class LandingFlow extends ConsumerWidget {
           icon: DynamicIcon(
             icon: Image.asset('assets/illustrations/bitcoin_coin.png'),
           ),
-          headline: 'Take full control and become unstoppable',
-          description:
-              'In Kumuly Pocket your money is really yours. Nobody can stop you from using it as you wish. Only you can access it through your 12 secret words.',
+          headline: copy.takeFullControlHeadline,
+          description: copy.takeFullControlDescription,
         ),
-        const LandingStoryScreen(
-            coverImageAssetPath: 'assets/images/mock_up_contact_list.png',
-            icon: Text(
-              '💕',
-              style: TextStyle(
-                fontSize: 24,
-              ),
-              textAlign: TextAlign.center,
+        LandingStoryScreen(
+          coverImageAssetPath: 'assets/images/mock_up_contact_list.png',
+          icon: const Text(
+            '💕',
+            style: TextStyle(
+              fontSize: 24,
             ),
-            headline: 'Get your family and friends on board',
-            description:
-                'Add contacts and spontaneously send, receive and request money without having to request or generate invoices. So simple, everyone can do it.'),
-        const LandingStoryScreen(
+            textAlign: TextAlign.center,
+          ),
+          headline: copy.getYourFamilyAndFriendsOnboardHeadline,
+          description: copy.getYourFamilyAndFriendsOnboardDescription,
+        ),
+        LandingStoryScreen(
           coverImageAssetPath: 'assets/images/mock_up_promo_redeem.png',
-          icon: Text(
+          icon: const Text(
             '🛍️',
             style: TextStyle(
               fontSize: 24,
             ),
           ),
-          headline: 'Directly spend sats on products and services',
-          description:
-              'You can find a constantly growing offering of merchants, products and services accepting bitcoin. No hassle, no selling. Just spend your sats directly and support the circular economy.',
+          headline: copy.directlySpendOnProductsAndServicesHeadline,
+          description: copy.directlySpendOnProductsAndServicesDescription,
         ),
-        const LandingStoryScreen(
+        LandingStoryScreen(
           coverImageAssetPath: 'assets/images/mock_up_promo.png',
-          icon: Text(
+          icon: const Text(
             '🏪',
             style: TextStyle(
               fontSize: 24,
             ),
           ),
-          headline: 'Let your business be discovered',
-          description:
-              'Swiftly access merchant features to grow your business on bitcoin. Make your products stand out with exclusive promotions and be embraced by the Bitcoin community.',
+          headline: copy.letYourBusinessBeDiscoveredHeadline,
+          description: copy.letYourBusinessBeDiscoveredDescription,
         ),
         const LandingScreen(),
       ],
